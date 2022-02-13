674b3edf5a129997372b47f97ea068a43e8b6b9e
Denis Yefremenko <denvirus83@mail.ru>
2022-02-13 13:22:01 +0400

    hw-events: GamePlay checked, interval 3000 ms

--- src/js/GamePlay.js
+++ src/js/GamePlay.js
@@ -1,14 +1,14 @@
-import greenVir from "../img/greenvirus.png";
-import violetVir from "../img/violetvirus.png";
-import dead from "../img/dead.png";
-import death from "../img/death.png";
-import knife from "../img/knife.png";
-import smile from "../img/smile.png";
-import werewolf from "../img/werewolf.png";
-import wolf from "../img/wolf.png";
-import netology from "../img/netology.svg";
-import winner from "../img/winner.jpg";
-import looser from "../img/looser.jpg";
+import greenVir from '../img/greenvirus.png';
+import violetVir from '../img/violetvirus.png';
+import dead from '../img/dead.png';
+import death from '../img/death.png';
+import knife from '../img/knife.png';
+import smile from '../img/smile.png';
+import werewolf from '../img/werewolf.png';
+import wolf from '../img/wolf.png';
+import netology from '../img/netology.svg';
+import winner from '../img/winner.jpg';
+import looser from '../img/looser.jpg';
 
 export default class GamePlay {
   constructor(container) {
@@ -26,7 +26,7 @@ export default class GamePlay {
     this.killEnemy = null;
     this.points = null;
     this.intervalChanging = null;
-    this.runInterval = "run";
+    this.runInterval = 'run';
     this.startButton = null;
     this.stopButton = null;
     this.finishGamePopup = null;
@@ -34,21 +34,20 @@ export default class GamePlay {
   }
 
   changeImage() {
-    this.scoreTable = this.container.querySelector(".score-table");
-    const miss = this.scoreTable.querySelectorAll(".score-table-data-cell")[0];
-    const hit = this.scoreTable.querySelectorAll(".score-table-data-cell")[1];
+    this.scoreTable = this.container.querySelector('.score-table');
+    const miss = this.scoreTable.querySelectorAll('.score-table-data-cell')[0];
+    const hit = this.scoreTable.querySelectorAll('.score-table-data-cell')[1];
     this.hitCell = hit;
     this.missCell = miss;
 
     if (this.killEnemy === false) {
-      this.missCell.textContent =
-        parseFloat(this.missCell.textContent) - this.points;
+      this.missCell.textContent = parseFloat(this.missCell.textContent) - this.points;
     }
 
     this.finishGame();
 
-    this.gameTable = this.container.querySelector(".field-table");
-    this.gameCells = this.gameTable.querySelectorAll(".field-table-data-cell");
+    this.gameTable = this.container.querySelector('.field-table');
+    this.gameCells = this.gameTable.querySelectorAll('.field-table-data-cell');
 
     this.newGameCellIndex = Math.floor(Math.random() * this.gameCells.length);
     if (this.newGameCellIndex === this.gameCellIndex) {
@@ -64,17 +63,17 @@ export default class GamePlay {
     this.randomEnemyIndex = Math.floor(Math.random() * this.enemies.length);
 
     const randomImage = this.gameCells[this.newGameCellIndex].querySelector(
-      ".field-table-cell-image"
+      '.field-table-cell-image',
     );
-    this.imageSrc = randomImage.getAttribute("src");
+    this.imageSrc = randomImage.getAttribute('src');
 
     const image = this.gameCells[this.gameCellIndex].querySelector(
-      ".field-table-cell-image"
+      '.field-table-cell-image',
     );
     randomImage.src = this.enemies[this.randomEnemyIndex];
     image.src = this.imageSrc;
 
-    const enemy = randomImage.getAttribute("src");
+    const enemy = randomImage.getAttribute('src');
 
     if (enemy === death) {
       this.points = 3;
@@ -88,14 +87,14 @@ export default class GamePlay {
   }
 
   startStopGame() {
-    this.startButton = document.getElementById("startButton");
-    this.stopButton = document.getElementById("stopButton");
+    this.startButton = document.getElementById('startButton');
+    this.stopButton = document.getElementById('stopButton');
     this.stopButtonListener1 = (event) => {
       event.preventDefault();
       clearInterval(this.intervalChanging);
-      this.runInterval = "run";
+      this.runInterval = 'run';
       const gameTableimages = this.gameTable.querySelectorAll(
-        ".field-table-cell-image"
+        '.field-table-cell-image',
       );
       for (let i = 0, len = gameTableimages.length; i < len; i++) {
         if (i % 2 === 0) {
@@ -112,44 +111,43 @@ export default class GamePlay {
     const startButtonListener1 = (event) => {
       event.preventDefault();
 
-      if (this.runInterval === "run") {
+      if (this.runInterval === 'run') {
         this.intervalChanging = setInterval(() => {
           this.changeImage();
         }, 3000);
 
-        this.stopButton.addEventListener("click", this.stopButtonListener1);
-        this.runInterval = "stop";
+        this.stopButton.addEventListener('click', this.stopButtonListener1);
+        this.runInterval = 'stop';
       }
     };
-    this.startButton.addEventListener("click", startButtonListener1);
+    this.startButton.addEventListener('click', startButtonListener1);
   }
 
   huntEnemies() {
-    this.gameTable = this.container.querySelector(".field-table");
+    this.gameTable = this.container.querySelector('.field-table');
     const gameTablelistener1 = (event) => {
       event.preventDefault();
       const aim = event.target;
-      const enemy = aim.getAttribute("src");
+      const enemy = aim.getAttribute('src');
 
       if (this.enemies.includes(enemy)) {
         this.killEnemy = true;
         aim.src = netology;
-        this.hitCell.textContent =
-          parseFloat(this.hitCell.textContent) - this.points;
+        this.hitCell.textContent = parseFloat(this.hitCell.textContent) - this.points;
       }
     };
-    this.gameTable.addEventListener("click", gameTablelistener1);
+    this.gameTable.addEventListener('click', gameTablelistener1);
 
-    this.finishGamePopup = this.gameTable.querySelector(".finish-game-popup");
+    this.finishGamePopup = this.gameTable.querySelector('.finish-game-popup');
 
     const closeButtonfinish = this.finishGamePopup.querySelector(
-      ".finish-game-popup-container-close-button"
+      '.finish-game-popup-container-close-button',
     );
     const gameTableimages = this.gameTable.querySelectorAll(
-      ".field-table-cell-image"
+      '.field-table-cell-image',
     );
     const closeButtonListener1 = (event) => {
-      this.finishGamePopup.style.display = "none";
+      this.finishGamePopup.style.display = 'none';
       for (let i = 0, len = gameTableimages.length; i < len; i++) {
         if (i % 2 === 0) {
           gameTableimages[i].src = greenVir;
@@ -157,40 +155,40 @@ export default class GamePlay {
           gameTableimages[i].src = violetVir;
         }
       }
-      this.runInterval = "run";
+      this.runInterval = 'run';
       this.hitCell.textContent = 30;
       this.missCell.textContent = 30;
       this.points = null;
     };
-    closeButtonfinish.addEventListener("click", closeButtonListener1);
+    closeButtonfinish.addEventListener('click', closeButtonListener1);
   }
 
   finishGame() {
     const finishGameimage = this.finishGamePopup.querySelector(
-      ".finish-game-popup-container-image"
+      '.finish-game-popup-container-image',
     );
     const finishGameText = this.finishGamePopup.querySelector(
-      ".finish-game-popup-container-text"
+      '.finish-game-popup-container-text',
     );
     if (parseFloat(this.missCell.textContent) <= 0) {
       clearInterval(this.intervalChanging);
       this.missCell.textContent = 0;
-      this.runInterval = "stop";
-      this.finishGamePopup.style.display = "block";
+      this.runInterval = 'stop';
+      this.finishGamePopup.style.display = 'block';
       finishGameimage.src = looser;
-      finishGameText.style.color = "red";
-      finishGameText.textContent = "YOU LOOSE!!";
-      this.stopButton.removeEventListener("click", this.stopButtonListener1);
+      finishGameText.style.color = 'red';
+      finishGameText.textContent = 'YOU LOOSE!!';
+      this.stopButton.removeEventListener('click', this.stopButtonListener1);
     }
     if (parseFloat(this.hitCell.textContent) <= 0) {
       clearInterval(this.intervalChanging);
       this.hitCell.textContent = 0;
-      this.runInterval = "stop";
-      this.finishGamePopup.style.display = "block";
+      this.runInterval = 'stop';
+      this.finishGamePopup.style.display = 'block';
       finishGameimage.src = winner;
-      finishGameText.style.color = "green";
-      finishGameText.textContent = "YOU WIN!!";
-      this.stopButton.removeEventListener("click", this.stopButtonListener1);
+      finishGameText.style.color = 'green';
+      finishGameText.textContent = 'YOU WIN!!';
+      this.stopButton.removeEventListener('click', this.stopButtonListener1);
     }
   }
 }
